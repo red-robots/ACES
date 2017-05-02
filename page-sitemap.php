65f70e8d188b0da2605cb7ee379511e67d3a745e
reneebellaworks <charleswells@Charless-MacBook-Pro-2.local>
2017-05-02 12:35:28 -0400

    sitemap

---
+++ page-sitemap.php
@@ -0,0 +1,46 @@
+<?php
+/**
+ * Template Name: Sitemap
+ *
+ * This is the template that displays all pages by default.
+ * Please note that this is the WordPress construct of pages
+ * and that other 'pages' on your WordPress site may use a
+ * different template.
+ *
+ * @link https://codex.wordpress.org/Template_Hierarchy
+ *
+ * @package ACStarter
+ */
+
+get_header(); ?>
+
+	<div id="primary" class="content-area">
+		<main id="main" class="site-main" role="main">
+
+			<?php
+			while ( have_posts() ) : the_post(); 
+
+if(is_page('about')) 
+			?>
+
+				<article id="post-<?php the_ID(); ?>" <?php post_class(); ?>>
+					<header class="entry-header">
+						<?php the_title( '<h1 class="entry-title">', '</h1>' ); ?>
+					</header><!-- .entry-header -->
+
+					<div class="entry-content">
+						<?php the_content(); ?>
+						<?php  wp_nav_menu( array( 'theme_location' => 'sitemap' ) ); ?>
+					</div><!-- .entry-content -->
+
+				</article><!-- #post-## -->
+
+			<?php endwhile; // End of the loop.
+			?>
+			
+
+		</main><!-- #main -->
+	</div><!-- #primary -->
+
+<?php
+get_footer();
